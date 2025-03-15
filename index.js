ba27f91ff11a8d99520e39486c21513054588b42
Oleg <olegmaslakov47@gmail.com>
2025-03-15 14:09:55 +0200

    Add some changes

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 'use strict';
 /**
- * 
- * @param {number} yourRate 
- * @param {number} opponentRate 
- * @param {number} differenceRate 
+ *
+ * @param {number} yourRate
+ * @param {number} opponentRate
+ * @param {number} differenceRate
  * @returns {Error}
  */
-function calculateNewRate(yourRate, opponentRate, differenceRate) {
+function calculateNewRate(yourRate, opponentRate) {
   if (
     typeof yourRate !== 'number' ||
     typeof opponentRate !== 'number' ||
-    typeof differenceRate !== 'number' ||
     yourRate < 0 ||
     opponentRate < 0
   ) {
@@ -19,6 +18,7 @@ function calculateNewRate(yourRate, opponentRate, differenceRate) {
   if (yourRate === 0) {
     return opponentRate;
   }
+  const differenceRate = yourRate - opponentRate;
   if (differenceRate >= 0 && differenceRate <= 2) {
     return Number(yourRate + 2).toFixed(1);
   } else if (differenceRate > 2 && differenceRate < 20) {
@@ -29,15 +29,14 @@ function calculateNewRate(yourRate, opponentRate, differenceRate) {
     return Number((opponentRate - yourRate + 5) / 3 + yourRate).toFixed(1);
   }
 }
-console.log(calculateNewRate(10, 10, 0));
-console.log(calculateNewRate(15, 14, 1));
-console.log(calculateNewRate(20, 18, 2));
-console.log(calculateNewRate(25, 10, 15));
-console.log(calculateNewRate(50, 30, 20));
-console.log(calculateNewRate('gfgfgfg', 30, 20));
+console.log(calculateNewRate(10, 10));
+console.log(calculateNewRate(15, 14));
+console.log(calculateNewRate(20, 18));
+console.log(calculateNewRate(25, 10));
+console.log(calculateNewRate(50, 30));
+console.log(calculateNewRate('gfgfgfg', 30));
 console.log(calculateNewRate(50, 'ffgfgfgfg', 20));
 console.log(calculateNewRate(30, 50, -20));
 console.log(calculateNewRate(0, 20, 0));
 console.log(calculateNewRate(0, 5, 0));
-console.log(calculateNewRate(0, 5, 'gfgfgfgfgf'));
 
